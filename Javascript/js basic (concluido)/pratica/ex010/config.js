6509711cc4db8cb75dac96fa89ccd35662b301c3
Amanda Eduarda <caduc57@gmail.com>
2023-01-04 17:40:52 -0300

    Ex010 Concluido

---
+++ Javascript/js basic (concluido)/pratica/ex010/config.js
@@ -0,0 +1,17 @@
+let resp = window.document.getElementById('acao');
+
+function Acao01(){
+    resp.innerHTML += `<p>Clicou no primeiro botao</p>`
+}
+
+function Acao02(){
+    resp.innerHTML += `<p>Clicou no segundo botao</p>`
+}
+
+function Acao03(){
+    resp.innerHTML += `<p>Clicou no terceiro botao</p>`
+}
+
+function Acao04(){
+    resp.innerHTML += `<p>Clicou no quarto botao</p>`
+}
