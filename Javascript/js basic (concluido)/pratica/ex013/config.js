c339e23f0b62101fd693396f4a8cf8c76e81d402
Amanda Eduarda <caduc57@gmail.com>
2023-01-04 19:13:13 -0300

    Ex013 adicionado o resultado iguais

--- Javascript/js basic (concluido)/pratica/ex013/config.js
+++ Javascript/js basic (concluido)/pratica/ex013/config.js
@@ -3,7 +3,16 @@ function Comparar() {
     msg = 0;
     let num1 = Number(prompt("Digite um numero"));
     let num2 = Number(prompt("Digite outro numero"));
-    if (num1 > num2){msg = num1} 
-    else {msg = num2}
-    result.innerHTML = (`<p> Analisando os valores <mark>${num1}</mark> e <mark>${num2}</mark>, O maior valor é <strong>${msg}</strong> </p>`)
+    if (num1 > num2){
+        msg = num1
+        result.innerHTML = (`<p> Analisando os valores <mark>${num1}</mark> e <mark>${num2}</mark>, O maior valor é <strong>${msg}</strong> </p>`)
+    } 
+    else if(num2 === num1){
+        result.innerHTML = (`<p> Analisando os valores <mark>${num1}</mark> e <mark>${num2}</mark>, ambos os valores sao <strong>IGUAIS</strong>`)
+    } 
+    else {
+        msg = num2
+        result.innerHTML = (`<p> Analisando os valores <mark>${num1}</mark> e <mark>${num2}</mark>, O maior valor é <strong>${msg}</strong> </p>`)
+    }
 }
+    
