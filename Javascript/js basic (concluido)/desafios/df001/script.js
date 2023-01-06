a47bc00b20b06a74be23b4e96070b8637351fcf8
Amanda Eduarda <caduc57@gmail.com>
2023-01-06 18:44:19 -0300

    Desafio 001 Concluido

---
+++ Javascript/js basic (concluido)/desafios/df001/script.js
@@ -0,0 +1,11 @@
+function botao1() {
+    alert("Voce clicou no 1º botao!")
+}
+
+function botao2() {
+    alert("Voce clicou no 2º botao!")
+}
+
+function botao3() {
+    alert("Voce clicou no 3º botao!")
+}
