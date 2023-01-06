e1d8f91368bc75f76fff8088d123dfc9be18f924
Amanda Eduarda <caduc57@gmail.com>
2023-01-06 18:53:35 -0300

    Df002 Concluido

---
+++ Javascript/js basic (concluido)/desafios/df002/script.js
@@ -0,0 +1,5 @@
+function inicio(){
+    let nome = prompt("Qual o seu nome?");
+    let idade = Number(prompt(`Ola, ${nome}! Quantos anos voce tem?`));
+    alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`);
+}
