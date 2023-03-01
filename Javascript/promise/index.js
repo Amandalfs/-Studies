1a89430d4de06616d1342f047871b78bc57d1e4e
Amandalfs <caduc57@gmail.com>
2023-03-01 13:37:12 -0300

    Criado a primeira promisse

--- Javascript/promise/index.js
+++ Javascript/promise/index.js
@@ -1,1 +1,22 @@
-const fs = require('fs')
+console.log('Pedir uber');
+
+let aceitar = false;
+
+const promesa = new Promise((resolve, reject)=>{
+    if(aceitar){
+        return resolve('Carro chegou')
+    } else {
+        return reject('pedido negado');
+
+    }
+});
+
+console.log('aguardando');
+
+promesa.then(result =>{
+    console.log(result)
+})
+.catch(result =>{
+    console.log(result)
+})
+.finally(()=>{console.log('Finalizada')}); 
