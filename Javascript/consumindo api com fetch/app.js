a57ad6df7262e09e69bd1db3263a8d651d73f9ac
Amandalfs <caduc57@gmail.com>
2023-03-27 11:34:43 -0300

    consumindo api com fetch no front

---
+++ Javascript/consumindo api com fetch/app.js
@@ -0,0 +1,17 @@
+const url="https://api-newlabs.cyclic.app/dados";
+
+/* const btn = document.querySelector('.btn')
+btn.addEventListener('click', ()=>{
+    getProducts();
+}) */
+
+function getProducts(){
+    fetch(url)
+        .then(result=> result.json())
+        .then((data) =>{
+            renderApiResult.textContent = JSON.stringify(data[0])
+        }
+        )
+        .catch(error=>console.error(error))
+}
+getProducts();
